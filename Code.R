20a2c260cfe26dcca18ce2393fcd7b92d96def02
Yannick <85198799+yannick0404@users.noreply.github.com>
2022-11-02 17:20:36 +0100

    Ein paar kleine Änderungen und Grafiken

--- Code.R
+++ Code.R
@@ -1,28 +1,70 @@
-setwd("~/Uni/5.WS2223/Fallstudien I/Projekt 2")
+### Projekt 2
+
+## Einlesen 
+# setwd("~/Uni/5.WS2223/Fallstudien I/Projekt 2")
 
 data<- read.table("mietspiegel2015.csv", header = TRUE)
 
+## Uebersicht
 str(data)
 
+summary(data)
+
+## nominale Daten zu Faktoren
+fact.mietspiegel <- function() {
+  if(is.factor(data$wohngut)) stop("schon fertig")
+  else {
+    data$wohngut <<- factor(data$wohngut, levels=c(0,1), labels=c("Schlecht", "Gut"))
+    data$wohnbest <<- factor(data$wohnbest, levels=c(0,1), labels=c("SchlechtOderGut", "Best"))
+    data$ww0 <<- factor(data$ww0, levels=c(0,1), labels=c("Ja","Nein"))
+    data$zh0 <<- factor(data$zh0, levels=c(0,1), labels=c("Ja","Nein"))
+    data$badkach0 <<- factor(data$badkach0, levels=c(0,1), labels=c("Gefliest","Ungefliest"))
+    data$badextra <<- factor(data$badextra, levels=c(0,1), labels=c("Normal","Gehoben"))
+    data$kueche <<- factor(data$kueche, levels=c(0,1), labels=c("Normal","Gehoben"))
+    data$bez <<- factor(data$bez)
+  }
+}
+fact.mietspiegel()
 
 
+# Grafiken
+attach(data)
 
-#summary(data)
-#plot(data$wfl, data$nmqm)
+plot(wfl, nmqm)
 
-#aggregate(nmqm ~ as.factor(bez), data = data, mean)
+hist(nmqm,  xlab = "Nettomiete pro qm", freq = F)
 
-#barplot(aggregate(nmqm ~ as.factor(bez), data = data, mean)[,2] )
+hist(wfl, xlab = " Wohnflaeche", freq = F)
 
-# Formatierung der nominalen Vektoren in Faktoren
-data$wohngut <- factor(data$wohngut, levels=c(0,1), labels=c("Schlecht", "Gut"))
-data$wohnbest <- factor(data$wohnbest, levels=c(0,1), labels=c("SchlechtOderGut", "Best"))
-data$ww0 <- factor(data$ww0, levels=c(0,1), labels=c("Ja","Nein"))
-data$zh0 <- factor(data$zh0, levels=c(0,1), labels=c("Ja","Nein"))
-data$badkach0 <- factor(data$badkach0, levels=c(0,1), labels=c("Gefliest","Ungefliest"))
-data$badextra <- factor(data$badextra, levels=c(0,1), labels=c("Normal","Gehoben"))
-data$kueche <- factor(data$kueche, levels=c(0,1), labels=c("Normal","Gehoben"))
-data$bez <- factor(data$bez)
+barplot(table(rooms))
+
+hist(bj, xlab = "Baujahr", freq = F)
+
+barplot(aggregate(nmqm ~ as.factor(bez),FUN =  mean)[,2],
+        main = "Bissel unuebersichtlich", xlab = "Bezirkname in Muenchen " )
+
+# entdummien
+barplot(table(as.numeric(wohngut) + as.numeric(wohnbest) +
+                as.numeric(wohnbest)-3), names.arg = c("Schlecht","Gut", "Best"),
+        xlab = "Lage")
+
+# barplot(table(wohngut))
+
+# barplot(table(wohnbest))
+
+
+barplot(table(ww0), xlab = "Warmwasserversorgung vom Vermieter gestellt")
+
+barplot(table(zh0), xlab = "Zentralheizung verfuegbar")
+
+barplot(table(badkach0), xlab = "Gefliestes/Gekacheltes Bad")
+
+barplot(table(badextra), xlab = "Ausstattung des Bades")
+
+barplot(table(kueche), xlab = "Ausstattung der Kueche")
+
+detach(data)
+
 
 data <- data[,-1]
 
