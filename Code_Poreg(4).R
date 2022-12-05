245c7d6dff6053d757ddc61542263aa5caa0fdee
Yannick <85198799+yannick0404@users.noreply.github.com>
2022-12-05 13:26:08 +0100

    Update Code_Poreg(4).R

--- Code_Poreg(4).R
+++ Code_Poreg(4).R
@@ -1,12 +1,35 @@
-setwd("C:/Users/Rudi2/iCloudDrive/Uni/Data Science B.Sc/Semester 5/FS1/Projekt 4")
+# setwd("C:/Users/Rudi2/iCloudDrive/Uni/Data Science B.Sc/Semester 5/FS1/Projekt 4")
+# setwd("~/Uni/5.WS2223/Fallstudien I/Projekt 4")
+
 data <- read.csv("Gesundheitszustand.csv")
 nrow(data)
-data <- subset(data, data$year==1991)
-data <- data[,-(1:2)]
+names(data)
+summary(data)
+
+
+anpassung <- function() {
+  if(ncol(data) != 28) return(warning("Daten sind bereits vorverarbeitet"))
+  else {
+    data <<- subset(data, data$year==1991)
+    data <<- data[,-c(1:2, 27:28)]
+  }
+}
+anpassung()
+
+
 colnames(data)
 nrow(data)
 head(data)
 
+
+
+table(data$hsat)
+table(data$handdum)
+table(data$hhinc)
+table(data$educ)
+
+
+# Modell
 x <- glm(docvis~., data=data, family = "poisson")
 summary(x)
 round(x$coefficients,4)
